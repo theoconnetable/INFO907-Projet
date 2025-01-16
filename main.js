2c04cc50e768094c06f3d83b41188f8b50a5c56d
Emilien39110 <emilien.boitouzet@hotmail.fr>
2025-01-16 15:55:37 +0100

    add navigation

--- main.js
+++ main.js
@@ -136,6 +136,14 @@ function getAllInstruments(level) {
     }
 }
 
+function getInstrumentsByKey(level) {
+    if(typeof level === "string") {
+        return [level];
+    } else {
+        return Object.values(level).flatMap(value => getAllInstruments(value))
+    }
+}
+
 function findParent(json, level, path = []) {
     //console.log(path);
     for (let key in json){
@@ -194,6 +202,35 @@ function getSubInstrument(json) {
     return res;
 }
 
+function displaySubInstruments(json) {
+    tab = getSubInstrument(json);
+    if (tab === undefined) {
+        console.log("No parent found");
+    } else {
+        const container = document.getElementById("children-container");
+        tab.forEach(element => {
+            // Créer un bouton
+            const parent = document.createElement("button");
+            parent.textContent = element;
+            parent.style.margin = "5px";
+
+            // Ajouter un événement clic au bouton
+            parent.addEventListener("click", () => {
+                while (container.firstChild) {
+                    container.firstChild.remove()
+                }
+                displaySubInstruments(json[element]);
+                displayInstruments(getAllInstruments(json[element]));
+            });
+        
+            // Ajouter le bouton au conteneur
+        container.appendChild(parent);
+        });
+    }
+}
+
+displaySubInstruments(instruments)
+
 function displayInstruments(instruments) {
     container = document.querySelector("#instruments-container")
 
@@ -201,7 +238,7 @@ function displayInstruments(instruments) {
         container.firstChild.remove()
     }
 
-    instruments.forEach(instrument => {
+    instruments.sort().forEach(instrument => {
         listItem = document.createElement("li");
         listItem.textContent = instrument;
         container.appendChild(listItem);
