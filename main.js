a60bfab3e6e563c8524263b9a2ab18fceb7336c1
baptistemonnier73 <monnierbaptiste01@gmail.com>
2025-01-16 12:47:39 +0100

    datas

---
+++ main.js
@@ -0,0 +1,151 @@
+/*attributs: 
+
+_Instrument_
+    vent
+        bois
+            anches
+                anche simple
+                    clarinette
+                        T sixte mineur en dessous
+                            Petite clarinette sopranino (lab)
+                        T tierce mineure en dessous
+                            Petite clarinette (mib)
+                        T seconde majeure en dessous
+                            Petite clarinette (ré)
+                        aucune transposition
+                            Clarinette soprano (ut)
+                        T seconde majeure au dessus
+                            Clarinette soprano (sib)
+                        T tierce mineure au dessus
+                            Clarinette soprano (la)
+                        T sixte majeure au dessus
+                            Clarinette alto (mib)
+                        T octave et seconde majeure au dessus
+                            Clarinette basse
+                        T octave et sixte majeure au dessus
+                            Clarinette contre-alto
+                        T 2 octaves et seconde majeure au dessus
+                            Clarinette contrebasse
+                    saxophone
+                        T seconde majeure en dessous
+                            Saxophone soprano
+                        T sixte majeure en dessous
+                            Saxophone alto
+                        T neuvième majeure en dessous
+                            Saxophone ténor
+                        T treizième majeure en dessous
+                            saxophone baryton
+                anche libre
+                    accordéon
+                anche double
+                    haubois
+        
+            embouchure
+                embouchures multiples
+                    Flute de pan
+
+                embouchure laterale
+                    Flute traversiere traditionnelle
+                    
+                embouchure ronde
+                    Xun (chinois)
+
+                embouchure encoche fixe
+                    Flute à encoche
+                
+                embouchure arete oblique
+                    Flute oblique
+                    
+                embouchure à conduit
+                    tubulaire
+                        se coulisse
+                            Flute à coulisse
+                        trous de jeu
+                            Flute à bec
+                    globulaire
+                        Ocarina
+                    sifflet
+                        Sifflet à nez
+        cuivre
+            cylindrique
+                coulisse
+                    si bémol
+                        tessiture aigue
+                            Trombone Soprano
+                        tessiture standard
+                            Trombone Ténor
+                        tessiture basse
+                            Trombone Basse
+                    mi bémol
+                        Trombone alto
+                pistons
+                    tessiture aigue
+                        Trompette
+                    tessiture basse
+                        Trombone à Piston
+            conico-cylindrique
+                pistons
+                    Si bémol
+                        Euphonium
+                    Fa
+                        Tuba Basse
+                    Ut
+                        Contretuba
+                palettes
+                    Cor
+
+    Percussion
+        idiophones
+            frappée
+                métalique
+                    hauteur réglable (tonale)
+                        avec résonnateur tubulaire 
+                            horizontale (frappe)
+                                Vibraphone
+                            Verticale (frappe)
+                                Carillon Tubulaire
+                        sans résonnateur tubulaire
+                            Glockenspiel
+                    hauteur fixe (atonale)
+                        percussion direct (ex : les Cymbales sentrchoque)
+                            Cymbales
+                        percussion indirect (ex : avec une baguette)
+                            baguette métalique
+                                Triangle
+                            baguette en bois
+                                Cloches
+                bois 
+                    hauteur réglable
+                        avec résonnateur tubulaire
+                            Marimba
+                        sans résonnateur tubulaire
+                            Xylophone
+                    hauteur fixe
+                        sans caisse résonance 
+                            Claves
+                        avec caisse résonance
+                            Woodblock
+            secouée
+                bois
+                    Maracas
+                métalique 
+                    bille à l'extérieur
+                        Cabassa
+                    bille à l'intérieur
+                        Shaker
+                    cymbalette 
+                        tambour de basque
+            raclée
+                bois contre bois
+                    Guiro
+                métal contre métal
+                    Cabassa
+        membranophones
+            tambours
+                main
+                    Congas
+                baguette
+                    Batterie
+                mailloche
+                    Timbales
+*/
